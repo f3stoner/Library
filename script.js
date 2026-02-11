e9b10d24e94415fcb16b18b5d8e3229dbe15f9c2
f3stoner <252489762+f3stoner@users.noreply.github.com>
2026-02-11 00:46:30 -0600

    add form functionality, card creation, delete

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const myLibrary = [];
+let myLibrary = [];
 const dialog = document.getElementById("newBook");
 const addButton = document.getElementById("addNew");
 const closeButton = document.getElementById("cancel");
-const newBook = document.getElementById("addNewBook")
-const bookInfoTester = document.querySelector(".bookInfoTester");
+const newBookForm = document.getElementById("addNewBook")
+const bookCards = document.getElementById("bookCards");
+
 
 addButton.addEventListener("click", () => {
     dialog.showModal();
@@ -11,16 +12,19 @@ addButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
     dialog.close();
+    newBookForm.reset();
 });
 
-newBook.addEventListener('submit', function(event) {
+newBookForm.addEventListener('submit', function(event) {
     event.preventDefault();
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById("pages").value;
     const read = document.getElementById("readStatus").value;
-
-    bookInfoTester.textContent = `Captured: ${title}, ${author}, ${pages}, ${read}`
+    
+    addBookToLibrary(title, author, pages, read);
+    dialog.close();
+    newBookForm.reset();
 });
 
 function Book(title, author, pages, read) {
@@ -31,10 +35,69 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    renderLibrary();
 }
 
-document.querySelector(bookCard.textContent(this.info()));
+function renderLibrary() {
+    clearLibrary();
+    for (const book of myLibrary) {
+        const newBookDiv = document.createElement("div");
+        newBookDiv.dataset.id = book.id;
+        newBookDiv.className = "book";
+        const bookTitle = document.createElement("div");
+        bookTitle.className = "bookTitle";
+        newBookDiv.append(bookTitle);
+        bookTitle.textContent = book.title;
+        const bookContent = document.createElement("div");
+        bookContent.className = "bookContent";
+        newBookDiv.append(bookContent);
+        bookContent.textContent = `By: ${book.author} Pages: ${book.pages} Read Status: ${book.read}`;
+        const bookActions = document.createElement("div");
+        bookActions.className = "bookActions";
+        newBookDiv.append(bookActions);
+        const toggleBtn = document.createElement("button");
+        toggleBtn.textContent = "Toggle Read Status";
+        toggleBtn.className = "toggle";
+        toggleBtn.addEventListener("click", function () {
+            toggleRead(book.id);
+        })
+        bookActions.append(toggleBtn);
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete Entry";
+        deleteBtn.className = "delete";
+        deleteBtn.addEventListener("click", function(){
+            deleteEntry(book.id);
+        })
+        bookActions.append(deleteBtn);
+        bookCards.append(newBookDiv);
+    }
+}
+
+function clearLibrary() {
+    while (bookCards.firstChild){
+    bookCards.removeChild(bookCards.firstChild);
+    }
+}
+
+function toggleRead(bookId) {
+
+}
+
+function deleteEntry(bookId) {
+   let newLibrary = [];
+   for (const book of myLibrary) {
+    if (book.id !== bookId){
+        newLibrary.push(book);
+    }
+    myLibrary = newLibrary;
+   }
+   renderLibrary();
+}
+
+
